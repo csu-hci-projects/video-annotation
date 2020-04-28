3e33d2af2e4f8fa696f06a095743d6bc4268cc07
KoalarBears <592060383@qq.com>
2020-04-28 04:51:45 -0600

    Add the vidoe annotation file to master

--- video-annotation/venv/Lib/tcl8.6/init.tcl
+++ video-annotation/venv/Lib/tcl8.6/init.tcl
@@ -8,6 +8,7 @@
 # Copyright (c) 1998-1999 Scriptics Corporation.
 # Copyright (c) 2004 by Kevin B. Kenny.  All rights reserved.
 #
+
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
